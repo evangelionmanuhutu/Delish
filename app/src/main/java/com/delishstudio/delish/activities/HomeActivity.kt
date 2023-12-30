90ad83e93f562cafaf635ed662a053b083e6cbb8
Evangelion Manuhutu <evangelion11.m@gmail.com>
2023-12-30 21:56:53 +0700

    Initial Commit

---
+++ app/src/main/java/com/delishstudio/delish/activities/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.delishstudio.delish.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.delishstudio.delish.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/delishstudio/delish/activities/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.delishstudio.delish.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.delishstudio.delish.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
