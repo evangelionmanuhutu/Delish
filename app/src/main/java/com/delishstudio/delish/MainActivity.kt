93c001573f9cd15b44ec004b6781ab8798d1dc75
Evangelion Manuhutu <evangelion11.m@gmail.com>
2023-12-30 22:11:13 +0700

    Hide Action Bar

--- app/src/main/java/com/delishstudio/delish/MainActivity.kt
+++ app/src/main/java/com/delishstudio/delish/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+
         setContentView(R.layout.activity_sign_in)
     }
 }
--- app/src/main/java/com/delishstudio/delish/activities/SignActivity.kt
+++ app/src/main/java/com/delishstudio/delish/activities/SignActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.delishstudio.delish.R
 
-class SignInActivity : AppCompatActivity() {
+class SignActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
